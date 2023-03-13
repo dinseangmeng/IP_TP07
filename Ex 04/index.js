463deb451206e947dce048e82bed4b82099181ec
dinseangmeng <dinseangmeng@gmail.com>
2023-03-13 20:33:14 +0700

    Add Ex 4

--- Ex 04/index.js
+++ Ex 04/index.js
@@ -2,28 +2,28 @@ require("dotenv").config();
 const express= require('express')
 const fs=require('fs')
 const app=express()
-
+const {KhmerDate} = require('./src/asset/js/KhmerDate')
+app.use(express.json())
 app.get('/asset',(req,res)=>{
     // console.log(req.query['assetPath']);
-    if(fs.existsSync(`./asset/${req.query['assetPath']}`)){
-        res.sendFile(req.query['assetPath'],{root:'./asset/'})
+    if(fs.existsSync(`./src/asset/${req.query['assetPath']}`)){
+        res.sendFile(req.query['assetPath'],{root:'./src/asset/'})
     }else{
         res.send("<h1>File Not found</h1>")
     }
 })
-app.get('/detail/:id',(req,res)=>[
-    fs.writeFile('./asset/Book_Library/id_data.js',`const id=${req.params.id}` , err => {
-        if (err) {
-            console.error(err)
-            return
-        }
-        res.sendFile('asset/Book_Library/view.html',{root:'./'})
+
+app.post('/getPastTime',(req,res)=>{
+    const {date}=req.body
+    const pastDate=new KhmerDate(new Date(date))
+    res.json({
+        msg:pastDate.getLabel()
     })
-    
-])
+})
+
 
 app.get('/',(req,res)=>{
-    res.sendFile('asset/Book_Library/index.html',{root:'./'})
+    res.sendFile('src/Template/index.html',{root:'./'})
 })
 
 app.listen(process.env.PORT ?? 3000,()=>{
